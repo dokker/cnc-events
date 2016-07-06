e36fab01622990b2fbe2f02fcbdaa4e5ee2c76cd
docker <dock3r@gmail.com>
2016-07-06 09:37:49 +0200

    Event archive theme

--- templates/archive-events.php
+++ templates/archive-events.php
@@ -8,21 +8,24 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
+<div class="mk-grid">
 <?php while (have_posts()) : the_post(); ?>
-	<article class="col-md-3 col-sm-4 <?php echo apply_filters('cnc_is_before_today', get_field('event_date')) ? 'older' : 'actual'; ?> <?php echo join( ' ', get_post_class() ); ?>">
+	<article class="mk-col-3-12 col-md-3 col-sm-4 <?php echo apply_filters('cnc_is_before_today', get_field('event_date')) ? 'older' : 'actual'; ?> <?php echo join( ' ', get_post_class() ); ?>">
 		<div class="entry-wrap">
 			<header>
 				<address><?php the_field('event_location_name'); ?></address>
-				<time>
-					<span class="date"><?php echo apply_filters('cnc_format_date_field', get_field('event_date'), 'Y/m/d'); ?></span>
-					<span class="time"><?php echo apply_filters('cnc_format_time_field', get_field('event_time')); ?></span>
+				<time class="date">
+					<a href="<?php the_permalink(); ?>"><span><?php echo apply_filters('cnc_format_date_field', get_field('event_date'), 'y/m/d'); ?></span></a>
 				</time>
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php echo apply_filters('cnc_limit_string', get_the_title(), 70); ?></a></h2>
+				<time class="time">
+					<span><?php echo apply_filters('cnc_format_time_field', get_field('event_time')); ?></span>
+				</time>
 			</header>
-			<?php apply_filters('cnc_smart_link', get_permalink(), __('Read more', 'cnc-events'), ''); ?>
 		</div>
 	</article>
 <?php endwhile; ?>
+</div>
 
 <?php //the_posts_navigation(); ?>
 <?php the_posts_pagination(['mid-size' => 5]); ?>
