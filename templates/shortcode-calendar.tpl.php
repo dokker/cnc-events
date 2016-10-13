c769c1558038c2395394272e46d7eb4e8b98405b
docker <dock3r@gmail.com>
2016-10-13 14:04:33 +0200

    add calendar frontend list basics

--- templates/shortcode-calendar.tpl.php
+++ templates/shortcode-calendar.tpl.php
@@ -1,4 +1,13 @@
 <div class="mk-grid">
+<ul class="cnc-events-calendar">
+<?php foreach($events_calendar as $daynum => $day): ?>
+	<li class="calendar--day<?php echo isset($day['today']) ? ' today' : ''; ?><?php echo isset($day['weekend']) ? ' weekend' : ''; ?><?php echo !empty($day['events']) ? ' event' : ''; ?>">
+		<span class="dayname"><?php echo $day['label'] ?></span>
+		<span class="daynum"><?php echo $daynum; ?></span>
+	</li>
+<?php endforeach; ?>
+</ul>
+
 <?php
 var_dump($events_calendar);
 ?>
