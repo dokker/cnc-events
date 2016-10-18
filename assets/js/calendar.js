54858501550e98a8868d1d8bc537bd85bd304184
docker <dock3r@gmail.com>
2016-10-18 14:28:13 +0200

    Add scroll to event details functionality

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,3 +1,27 @@
 (function($) {
-	
+    function scrollToElement ($target_element, duration, offset) {
+      var destination = $target_element.offset().top - offset;
+      var itemHeight = $target_element.height();
+      $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, duration, "easeInOutCubic");
+    }
+
+	function hide_day_events() {
+		$('.calendar--event-day').hide();
+	}
+
+	function show_day_event(daynum) {
+		$('.calendar--details *[data-daynum="' + daynum + '"]').show(0, function() {
+			$scrollTarget = $('.calendar--details');
+			scrollToElement($scrollTarget, 500, 200);
+		});
+	}
+	$(".cnc-events-calendar .calendar--day").click(function() {
+		if($(this).hasClass('active') == false) {
+			$('.cnc-events-calendar .calendar--day').removeClass('active');
+			$(this).addClass('active');
+			hide_day_events();
+			var daynum = $(this).data('daynum');
+			show_day_event(daynum);
+		}
+	});
 })(jQuery);
--- templates/shortcode-calendar.tpl.php
+++ templates/shortcode-calendar.tpl.php
@@ -5,16 +5,19 @@
 <p class="calendar--date"><span class="next-month calendar--pager"><</span><span class="calendar-date-label"><?php echo date_i18n('Y F', time()); ?></span><span class="prev-month calendar--pager">></span></p>
 <ul class="cnc-events-calendar">
 <?php foreach($events_calendar as $daynum => $day): ?>
-	<li class="calendar--day<?php echo isset($day['today']) ? ' today' : ''; ?><?php echo isset($day['weekend']) ? ' weekend' : ''; ?><?php echo !empty($day['events']) ? ' event' : ''; ?>">
+	<li data-daynum="day-<?php echo $daynum; ?>"
+		class="calendar--day<?php echo isset($day['today']) ? ' today' : ''; ?>
+		<?php echo isset($day['weekend']) ? ' weekend' : ''; ?>
+		<?php echo !empty($day['events']) ? ' event' : ''; ?>">
 		<span class="dayname"><?php echo $day['dow'] ?></span>
 		<span class="daynum"><?php echo $daynum; ?></span>
 	</li>
 <?php endforeach; ?>
 </ul>
 <div class="calendar--details">
-	<?php foreach($events_calendar as $event_day): ?>
+	<?php foreach($events_calendar as $daynum => $event_day): ?>
 		<?php if(!empty($event_day['events'])): ?>
-			<div class="calendar--event-day">
+			<div data-daynum="day-<?php echo $daynum; ?>" class="calendar--event-day">
 				<?php foreach($event_day['events'] as $event): ?>
 					<p class="date"><?php the_field('event_date_start', $event->ID); ?> - <?php the_field('event_date_end', $event->ID); ?></p>
 					<p class="title"><a href="<?php echo get_permalink($event->ID); ?>"><?php echo $event->post_title; ?></a></p>
