d01af446fccc9dce4daf8701d7d2c1522bcd6df2
docker <dock3r@gmail.com>
2017-02-27 12:39:07 +0100

    Add related product button

--- templates/single-event.php
+++ templates/single-event.php
@@ -24,6 +24,12 @@
 						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
 					</div>
 				<?php endif; ?>
+				<?php $related_product = get_field('related_product');
+				if( !empty($related_product) ): ?>
+					<div class="related-product-container">
+						<a href="<?php echo get_permalink($related_product); ?>" class="single_add_to_cart_button shop-skin-btn shop-flat-btn alt related-product-button"><i class="mk-moon-cart-plus"></i><?php echo get_the_title($related_product); ?></a>
+					</div>
+				<?php endif; ?>
 			</article>
 		</div>
 		<aside id="mk-sidebar" class="mk-builtin">
