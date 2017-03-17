5b17e6a6c8a3314e900606772eac5d2d28866323
docker <dock3r@gmail.com>
2017-03-17 10:54:36 +0100

    Add gap to event archive top and bottom

--- templates/archive-events.php
+++ templates/archive-events.php
@@ -1,31 +1,33 @@
 <?php get_header(); ?>
 <?php get_template_part('content', 'page'); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php // get_search_form(); ?>
-<?php endif; ?>
+<div class="cnc-event-archive">
+    <?php if (!have_posts()) : ?>
+      <div class="alert alert-warning">
+        <?php _e('Sorry, no results were found.', 'sage'); ?>
+      </div>
+      <?php // get_search_form(); ?>
+    <?php endif; ?>
 
-<?php
-    switch (get_field('cnc-event-archive-style', 'option')) {
-    	case 'minimal':
-    		echo do_shortcode('[cnc_events_list style="minimal"]');
-    		break;
-    	case 'simple':
-    		echo do_shortcode('[cnc_events_list style="simple"]');
-    		break;
-    	case 'calendar':
-    		echo do_shortcode('[cnc_events_list style="calendar"]');
-    		break;
-    	default:
-    		$list_style = 'minimal';
-    		break;
-    }
-?>
+    <?php
+        switch (get_field('cnc-event-archive-style', 'option')) {
+        	case 'minimal':
+        		echo do_shortcode('[cnc_events_list style="minimal"]');
+        		break;
+        	case 'simple':
+        		echo do_shortcode('[cnc_events_list style="simple"]');
+        		break;
+        	case 'calendar':
+        		echo do_shortcode('[cnc_events_list style="calendar"]');
+        		break;
+        	default:
+        		$list_style = 'minimal';
+        		break;
+        }
+    ?>
 
-<?php //the_posts_navigation(); ?>
-<?php the_posts_pagination(['mid-size' => 5]); ?>
+    <?php //the_posts_navigation(); ?>
+    <?php the_posts_pagination(['mid-size' => 5]); ?>
+</div>
 
 <?php get_footer(); ?>
