a4dfecd0c0243cdfcf6d630cdde826784705201b
docker <dock3r@gmail.com>
2017-03-29 12:57:19 +0200

    Move related product button to meta level

--- templates/single-event.php
+++ templates/single-event.php
@@ -3,7 +3,7 @@
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
 <div id="theme-page" class="master-holder clear">
-	<div class="mk-grid theme-page-wrapper right-layout">
+	<div class="mk-grid theme-page-wrapper full-layout">
 		<div class="theme-content">
 			<article id="<?php the_ID(); ?>" itemscope="itemscope" itemtype="https://schema.org/Event" itemprop="event">
 				<div class="featured-area">
@@ -12,22 +12,26 @@
 				<?php else: ?>
 				<?php endif; ?>
 				</div>
-				<address><?php the_field('event_location_name'); ?></address>
-				<time>
-					<?php echo apply_filters('cnc_format_date_field', get_field('event_date_start', false, false), 'Y.m.d.'); ?>
-					<?php echo (!empty(get_field('event_date_end'))) ? ' - ' . apply_filters('cnc_format_date_field', get_field('event_date_end', false, false), 'Y.m.d.') : ''; ?>
-				</time>
+				<div class="event-head">
+					<div class="event-details">
+						<address><?php the_field('event_location_name'); ?></address>
+						<time>
+							<?php echo apply_filters('cnc_format_date_field', get_field('event_date_start', false, false), 'Y.m.d.'); ?>
+							<?php echo (!empty(get_field('event_date_end'))) ? ' - ' . apply_filters('cnc_format_date_field', get_field('event_date_end', false, false), 'Y.m.d.') : ''; ?>
+						</time>
+					</div>
+					<?php $related_product = get_field('related_product');
+					if( !empty($related_product) ): ?>
+						<div class="related-product-container">
+							<a href="<?php echo get_permalink($related_product); ?>" class="single_add_to_cart_button shop-skin-btn shop-flat-btn alt related-product-button"><i class="mk-moon-cart-plus"></i><?php echo get_the_title($related_product); ?></a>
+						</div>
+					<?php endif; ?>
+				</div>
 				<div class="content"><?php the_content( $more_link_text, $strip_teaser ); ?></div>
 				<?php $location = get_field('event_location');
 				if( !empty($location) ): ?>
 					<div class="acf-map">
 						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-					</div>
-				<?php endif; ?>
-				<?php $related_product = get_field('related_product');
-				if( !empty($related_product) ): ?>
-					<div class="related-product-container">
-						<a href="<?php echo get_permalink($related_product); ?>" class="single_add_to_cart_button shop-skin-btn shop-flat-btn alt related-product-button"><i class="mk-moon-cart-plus"></i><?php echo get_the_title($related_product); ?></a>
 					</div>
 				<?php endif; ?>
 			</article>
