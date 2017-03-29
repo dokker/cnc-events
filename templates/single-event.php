f1e1ca46e1dbbcbefd2c1ab42680acbd889da98a
docker <dock3r@gmail.com>
2017-03-29 14:56:58 +0200

    fix cart icon on related product button

--- templates/single-event.php
+++ templates/single-event.php
@@ -23,7 +23,7 @@
 					<?php $related_product = get_field('related_product');
 					if( !empty($related_product) ): ?>
 						<div class="related-product-container">
-							<a href="<?php echo get_permalink($related_product); ?>" class="single_add_to_cart_button shop-skin-btn shop-flat-btn alt related-product-button"><i class="mk-moon-cart-plus"></i><?php echo get_the_title($related_product); ?></a>
+							<a href="<?php echo get_permalink($related_product); ?>" class="single_add_to_cart_button shop-skin-btn shop-flat-btn alt related-product-button"><i class="fa fa-cart-plus"></i><?php echo get_the_title($related_product); ?></a>
 						</div>
 					<?php endif; ?>
 				</div>
